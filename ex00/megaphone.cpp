1ded508fe795363d10c8a925a7864bbfc81f6858
Bernardo <bercbranco@gmail.com>
2024-01-21 22:38:13 +0000

    Changing variable declaration

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,18 +3,15 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
-
 	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 		return (0);
 	}
 
-	for (i = 1; i < argc; i++)
+	for (int i = 1; i < argc; i++)
 	{
-		j = 0;
+		int j = 0;
 		while (argv[i][j])
 		{
 			argv[i][j] = toupper(argv[i][j]);
